a4e4886c248158aa4198dbcad542923bf0df4f6d
alexandre.chaves <alexandre.chaves@LAD0416.cwbdis.pucpr.br>
2017-04-04 22:07:37 -0300

    Multicast Base Funcionando

---
+++ src/multicast/Coordenador.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multicast;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+/**
+ *
+ * @author alexandre.chaves
+ */
+public class Coordenador {
+    
+    
+
+	public static void main(String [] args) throws IOException{
+		MulticastSocket remSocket = new MulticastSocket();
+		byte[] send;
+		String message = "hello group";
+		send = message.getBytes();
+		String group = "224.0.0.1";
+		DatagramPacket pacote = new DatagramPacket(send, send.length,InetAddress.getByName(group) , 3333);
+		remSocket.send(pacote);
+				
+	}
+	
+	
+}
+
